6003045f27d3d6551eda7f7b3f421099ce4ea849
Miguel Angel García Mejía <mgarciammej@gmail.com>
2025-08-11 12:15:41 -0600

    prueba

---
+++ Components/Hello.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export function Hello(){
+    return (
+        <View style={{ padding: 20, backgroundColor: '#f0f0f0' }}>
+            <Text>
+                Hola desde el componente
+            </Text>
+        </View>
+    )
+}
+
---
+++ Components/RoundedButton.tsx
@@ -0,0 +1,10 @@
+import { Pressable } from "react-native";
+import React from "react";
+
+export function RoundedButton({children}: React.PropsWithChildren<{}>) {
+    return (
+        <Pressable style={{backgroundColor: '#6200ee', padding: 10, borderRadius: 30}}>
+            {children}
+        </Pressable>
+    )
+}
