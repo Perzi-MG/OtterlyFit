4de6e40558ba886493330a09b97f67e3b0426e12
Perzi-MG <mgarciammej@gmail.com>
2025-08-11 12:44:03 -0600

    SVG Options

--- Components/RoundedButton.tsx
+++ Components/RoundedButton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export function RoundedButton({children}: React.PropsWithChildren<{}>) {
     return (
-        <Pressable style={{backgroundColor: '#6200ee', padding: 10, borderRadius: 30}}>
+        <Pressable style={{backgroundColor: '#6200ee', padding: 10, borderRadius: 30, borderColor: '#fff'}}>
             {children}
         </Pressable>
     )
