707b83a2dca9cd1bfc0e6c96aafcf053a0b63e3b
JoseAraiza2402 <jaraiza24@alumnos.uaq.mx>
2025-08-12 14:09:46 -0600

    blur

--- Components/BlurBackground.tsx
+++ Components/BlurBackground.tsx
@@ -2,7 +2,7 @@ import { View } from "react-native";
 
 export function BlurBackground() {
     return (
-        <View className="">
+        <View className="h-32 w-full bg-[linear-gradient(to_right,#E3E2A9_0%,#BFC8B5_21%,#C9CBD7_100%)]">
 
         </View>
     )
